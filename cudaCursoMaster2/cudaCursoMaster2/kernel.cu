bc0a4b4242f8c027a21471496ea923c23476cf88
Nautilus com <nautilus_com@yahoo.com>
2021-01-10 06:16:26 -0600

    arquitectura utilizada por cuda

---
+++ cudaCursoMaster2/cudaCursoMaster2/kernel.cu
@@ -0,0 +1,21 @@
+
+//-------------------------------------219 Understadn the device better---------------------------------------
+
+/* computer architectures classification
+		-SISD - Single instruction single data
+		-SIMD - single instruction multiple data
+		-MISD - multiple instruction single data
+		-MIMD - multiple instruction multiple data 
+	CUDA se basa en SIMD
+		-SIMT - single instruction multiple threads
+
+*/
+
+
+
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//
+//#include <stdio.h>
+
+
