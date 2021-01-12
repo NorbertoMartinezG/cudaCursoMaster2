9613b43dfff45fef4f57dae758c9ccda7edf3dd4
Nautilus com <nautilus_com@yahoo.com>
2021-01-12 15:06:51 -0600

    warp divergence

--- cudaCursoMaster2/cudaCursoMaster2/kernel.cu
+++ cudaCursoMaster2/cudaCursoMaster2/kernel.cu
@@ -33,36 +33,120 @@
 */
 
 // EJEMPLO DE USO DE BLOQUES CUANDO SE UTILIZAN 40 HILOS
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//__global__ void print_details_of_warps()
+//{
+//	int gid = blockIdx.y * gridDim.x * blockDim.x + blockIdx.x * blockDim.x + threadIdx.x;
+//
+//	int warp_id = threadIdx.x / 32;
+//
+//	int gbid = blockIdx.y * gridDim.x + blockIdx.x;
+//
+//	printf( "tid: %d, bid.x: %d, bid.y: %d, gid: %d, warp_id: %d, gbid: %d \n",
+//		threadIdx.x, blockIdx.x, blockIdx.y, gid, warp_id, gbid);
+//}
+//
+//
+//int main(int argc, char** argv) {
+//
+//	dim3 block_size(42);
+//	dim3 grid_size(2, 2);
+//
+//	print_details_of_warps << <grid_size, block_size >> > ();
+//	cudaDeviceSynchronize();
+//
+//	cudaDeviceReset();
+//	return EXIT_SUCCESS;
+//
+//}
 
-#include <stdio.h>
-#include <stdlib.h>
+//------------------------------------- 221 Warp divergence ---------------------------------------
+/*
+	- forzar a algunos hilos en el warp para ejecutar diferentes instrucciones
+	- la eficiencia de las ramas se puede medir para un kernel utilizando la herramienta de creacion de perfiles nvprof.
 
-__global__ void print_details_of_warps()
-{
-	int gid = blockIdx.y * gridDim.x * blockDim.x + blockIdx.x * blockDim.x + threadIdx.x;
+*/
 
-	int warp_id = threadIdx.x / 32;
-
-	int gbid = blockIdx.y * gridDim.x + blockIdx.x;
-
-	printf( "tid: %d, bid.x: %d, bid.y: %d, gid: %d, warp_id: %d, gbid: %d \n",
-		threadIdx.x, blockIdx.x, blockIdx.y, gid, warp_id, gbid);
-}
-
-
-int main(int argc, char** argv) {
-
-	dim3 block_size(42);
-	dim3 grid_size(2, 2);
-
-	print_details_of_warps << <grid_size, block_size >> > ();
-	cudaDeviceSynchronize();
-
-	cudaDeviceReset();
-	return EXIT_SUCCESS;
-
-}
-
+//// EJEMPLO DE DIVERGENCIA
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//#include "cuda.h"
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+////#include "cuda_common.cuh"
+//
+//// EJECUTAR EN TERMINAL******************************
+//__global__ void code_without_divergence() // asignacion de valores a A y B basados en WARP
+//{
+//	int gid = blockIdx.x * blockDim.x + threadIdx.x;  // id de thread global
+//
+//	float a, b;
+//	a = b = 0;
+//
+//	int warp_id = gid / 32;
+//
+//	if (warp_id % 2 == 0)
+//	{
+//		a = 100.0;
+//		b = 50.0;
+//
+//
+//	}
+//	else
+//	{
+//		a = 200;
+//		b = 75;
+//	}
+//
+//}
+//
+//__global__ void code_with_divergence() // asignacion de valores a A y B basados en THREAD
+//{
+//	int gid = blockIdx.x * blockDim.x + threadIdx.x;  // id de thread global
+//
+//	float a, b;
+//	a = b = 0;
+//
+//	if (gid % 2 == 0)
+//	{
+//		a = 100.0;
+//		b = 50.0;
+//
+//
+//	}
+//	else
+//	{
+//		a = 200;
+//		b = 75;
+//	}
+//
+//}
+//
+//
+//int main(int argc, char** argv)
+//{
+//	printf("\n---------------------------WARP DIVERGENCE EXAMPLE----------------------\n\n");
+//
+//	int size = 1 << 22;
+//
+//	dim3 block_size(128);
+//	dim3 grid_size((size + block_size.x - 1) / block_size.x);
+//
+//	code_without_divergence << <grid_size, block_size >> > ();
+//	cudaDeviceSynchronize();
+//
+//	code_with_divergence << <grid_size, block_size >> > ();
+//	cudaDeviceSynchronize();
+//
+//
+//	cudaDeviceReset();
+//	return 0;
+//}
 
