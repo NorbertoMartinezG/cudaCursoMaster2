c0e074eb3ad9df7c6f5fa8e36f0115e7bf39d5d7
Nautilus com <nautilus_com@yahoo.com>
2021-02-03 20:28:50 -0600

    Parallel reduction as warp unrolling

--- cudaCursoMaster2/cudaCursoMaster2/kernel.cu
+++ cudaCursoMaster2/cudaCursoMaster2/kernel.cu
@@ -436,6 +436,139 @@ the body is written in code multiple times.
 
 */
 
+////Ejemplo suma elementos de un vector  (reduccion paralela)
+//
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//
+//#include "common.h"
+//#include "cuda_common.cuh"
+//
+//__global__ void reduction_unrolling_bloks2(int* input, int* temp, int size)
+//{
+//	int tid = threadIdx.x;
+//	int BLOCK_OFFSET = blockIdx.x * blockDim.x * 2;
+//	int index = BLOCK_OFFSET + tid;
+//	int* i_data = input + BLOCK_OFFSET;
+//
+//	if ((index + blockDim.x) < size)
+//	{
+//		input[index] += input[index + blockDim.x];
+//	}
+//	__syncthreads();
+//
+//	for (int offset = blockDim.x / 2; offset > 0; offset = offset / 2)
+//		{
+//			if (tid < offset)
+//			{
+//				i_data[tid] += i_data[tid + offset];
+//			}
+//			__syncthreads();
+//		}
+//	
+//		if (tid == 0)
+//		{
+//			temp[blockIdx.x] = i_data[0];
+//		}
+//
+//}
+//
+//__global__ void reduction_unrolling_bloks4(int* input, int* temp, int size)
+//{
+//	int tid = threadIdx.x;
+//	int BLOCK_OFFSET = blockIdx.x * blockDim.x * 4;
+//	int index = BLOCK_OFFSET + tid;
+//	int* i_data = input + BLOCK_OFFSET;
+//
+//	if ((index + 3 * blockDim.x) < size)
+//	{
+//		int a1 = input[index];
+//		int a2 = input[index + blockDim.x];
+//		int a3 = input[index + 2 * blockDim.x];
+//		int a4 = input[index + 3 * blockDim.x];
+//		input[index] = a1 + a2 + a3 + a4;
+//	}
+//	__syncthreads();
+//
+//	for (int offset = blockDim.x / 2; offset > 0; offset = offset / 2)
+//	{
+//		if (tid < offset)
+//		{
+//			i_data[tid] += i_data[tid + offset];
+//		}
+//		__syncthreads();
+//	}
+//
+//	if (tid == 0)
+//	{
+//		temp[blockIdx.x] = i_data[0];
+//	}
+//
+//}
+//
+//int main(int argc, char** argv)
+//{
+//	printf("Running neighbored pairs reduction kernel \n");
+//
+//	int size = 1 << 27; // 128 Mb of data
+//	int byte_size = size * sizeof(int);
+//	int block_size = 128;
+//
+//	int* h_input, * h_ref;
+//	h_input = (int*)malloc(byte_size);
+//
+//	initialize(h_input, size, INIT_RANDOM);
+//
+//	//get the reduction result from cpu
+//	int cpu_result = reduction_cpu(h_input, size);
+//
+//	dim3 block(block_size);
+//	dim3 grid((size / block.x) / 4); // CAMBIAR ESTE VALOR DEPENDIENDO DEL KERNEL QUE SE UTILIZA YA SEA 2 O 4
+//
+//	printf("kernel launch parameters | grid.x : %d, block.x : %d \n\n", grid.x, block.x);
+//
+//	int temp_array_byte_size = sizeof(int) * grid.x;
+//	h_ref = (int*)malloc(temp_array_byte_size);
+//
+//	int* d_input, * d_temp;
+//
+//	gpuErrchk(cudaMalloc((void**)&d_input, byte_size));
+//	gpuErrchk(cudaMalloc((void**)&d_temp, temp_array_byte_size));
+//
+//	gpuErrchk(cudaMemset(d_temp, 0, temp_array_byte_size)); // establece valor inicial en 0 
+//	gpuErrchk(cudaMemcpy(d_input, h_input, byte_size, cudaMemcpyHostToDevice));
+//
+//	reduction_unrolling_bloks4 << <grid, block >> > (d_input, d_temp, size);
+//
+//	gpuErrchk(cudaDeviceSynchronize());
+//
+//	cudaMemcpy(h_ref, d_temp, temp_array_byte_size, cudaMemcpyDeviceToHost);
+//
+//	int gpu_result = 0;
+//	for (int i = 0; i < grid.x; i++)
+//	{
+//		gpu_result += h_ref[i];
+//	}
+//
+//	//validity check
+//	compare_results(gpu_result, cpu_result);
+//
+//	gpuErrchk(cudaFree(d_temp));
+//	gpuErrchk(cudaFree(d_input));
+//
+//	free(h_ref);
+//	free(h_input);
+//
+//
+//	gpuErrchk(cudaDeviceReset());
+//	return 0;
+//}
+
+//---------------------------------- 229 Parallel reduction with loop unrolling --------------------------------
+
 //Ejemplo suma elementos de un vector  (reduccion paralela)
 
 #include <stdio.h>
@@ -461,18 +594,18 @@ __global__ void reduction_unrolling_bloks2(int* input, int* temp, int size)
 	__syncthreads();
 
 	for (int offset = blockDim.x / 2; offset > 0; offset = offset / 2)
+	{
+		if (tid < offset)
 		{
-			if (tid < offset)
-			{
-				i_data[tid] += i_data[tid + offset];
-			}
-			__syncthreads();
+			i_data[tid] += i_data[tid + offset];
 		}
-	
-		if (tid == 0)
-		{
-			temp[blockIdx.x] = i_data[0];
-		}
+		__syncthreads();
+	}
+
+	if (tid == 0)
+	{
+		temp[blockIdx.x] = i_data[0];
+	}
 
 }
 
@@ -493,7 +626,7 @@ __global__ void reduction_unrolling_bloks4(int* input, int* temp, int size)
 	}
 	__syncthreads();
 
-	for (int offset = blockDim.x / 2; offset > 0; offset = offset / 2)
+	for (int offset = blockDim.x / 2; offset >= 64; offset = offset / 2)
 	{
 		if (tid < offset)
 		{
@@ -501,6 +634,23 @@ __global__ void reduction_unrolling_bloks4(int* input, int* temp, int size)
 		}
 		__syncthreads();
 	}
+
+	if (tid < 32)
+	{
+		/*
+		  El modificador volatile quiere decir: Esta variable no sólo modifica tu programa sino alguien más.
+		  La pregunta es: ¿quién más cambia las variables de tu programa? Pues, un componente de hardware, 
+		  por ejemplo. Cuando escribes un controlador de hardware, entonces usas volatile bastante a menudo. 
+		*/
+		volatile int* vsmen = i_data;
+		vsmen[tid] += vsmen[tid + 32];
+		vsmen[tid] += vsmen[tid + 16];
+		vsmen[tid] += vsmen[tid + 8];
+		vsmen[tid] += vsmen[tid + 4];
+		vsmen[tid] += vsmen[tid + 2];
+		vsmen[tid] += vsmen[tid + 1];
+	}
+
 
 	if (tid == 0)
 	{
@@ -526,7 +676,7 @@ int main(int argc, char** argv)
 	int cpu_result = reduction_cpu(h_input, size);
 
 	dim3 block(block_size);
-	dim3 grid((size / block.x) / 4); // CAMBIAR ESTE VALOR DEPENDIENDO DEL KERNEL QUE SE UTILIZA YA SEA 2 O 4
+	dim3 grid((size / block.x) / 2); // CAMBIAR ESTE VALOR DEPENDIENDO DEL KERNEL QUE SE UTILIZA YA SEA 2 O 4
 
 	printf("kernel launch parameters | grid.x : %d, block.x : %d \n\n", grid.x, block.x);
 
@@ -541,7 +691,7 @@ int main(int argc, char** argv)
 	gpuErrchk(cudaMemset(d_temp, 0, temp_array_byte_size)); // establece valor inicial en 0 
 	gpuErrchk(cudaMemcpy(d_input, h_input, byte_size, cudaMemcpyHostToDevice));
 
-	reduction_unrolling_bloks4 << <grid, block >> > (d_input, d_temp, size);
+	reduction_unrolling_bloks2 << <grid, block >> > (d_input, d_temp, size); // USAR KERNEL DEPENDIENDO DEL VALOR QUE DIVIDE EL VALOR DE GRID
 
 	gpuErrchk(cudaDeviceSynchronize());
 
